b04b3cdea4ac3c7efb39b61e3132937add440789
sinfaur <123294760+sinfaur@users.noreply.github.com>
2023-08-31 21:39:13 +0100

    ewa
    waa

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that shows the Content-Length from a HTTP request
+# A Bash script that takes in a URL, sends a request and displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
